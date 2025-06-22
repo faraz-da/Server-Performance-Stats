149c50e435da3ddd45eb0108269f685d91f0320b
Practice User <practice@example.com>
2025-06-22 09:14:44 -0400

    code complete

--- server-stats.sh
+++ server-stats.sh
@@ -3,3 +3,40 @@
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | \
            sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
            awk '{print 100 - $1"%"}')
+echo "---------------------"
+echo "Cpu usage:$cpu_usage"
+echo "---------------------"
+
+read total used <<< $(free | awk 'NR==2{print $2, $3}')
+percent=$(awk "BEGIN {printf \"%.2f\", $used/$total*100}")
+
+Total_memory=$((total/1024))
+Used_memory=$((used/1024))
+
+echo "Memory Usage Info (/):"
+echo "---------------------"
+echo "Total memory: $((total/1024)) MB"
+echo "Used memory : $((used/1024)) MB"
+echo "Used percent: $percent %"
+
+read total used free percent <<< $(df / | awk 'NR==2 {print $2, $3, $4, $5}')
+
+total_disk=$((total))
+used_disk=$((used))
+free_disk= $((free))
+
+echo "---------------------"
+echo "Disk Usage Info (/):"
+echo "---------------------"
+echo "Total: $((total)) KB"
+echo "Used : $((used)) KB"
+echo "Free : $((free)) KB"
+echo "Used Percent: $percent"
+echo "---------------------"
+
+
+echo "Top 5 CPU-consuming processes:"
+ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6
+echo ""
+echo "Top 5 Memory-consuming processes:"
+ps -eo pid,comm,%mem --sort=-%mem | head -n 6
